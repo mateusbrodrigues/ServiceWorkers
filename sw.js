2505a4dee7fa2bf2b07a4835e1262b12e2ca5d3b
Mateus Rodrigues <mateusborges090@gmail.com>
2024-07-27 16:48:11 -0300

    fix js cache

--- sw.js
+++ sw.js
@@ -1,24 +1,12 @@
 self.addEventListener('install', (event) => {
   console.log(`[Service Worker] install event lifecycle!!`);
   self.skipWaiting(); // don't wait for installation just activate it
-  event.waitUntil(
-    caches
-      .open('sw-cache-v1')
-      .then(async (cache) =>
-        cache.addAll([
-          '/',
-          '/index.html',
-          'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.fluid.classless.min.css',
-          './app.js',
-          '/images/cat.svg',
-          '/images/dog.svg',
-        ])
-      )
-  );
+  event.waitUntil(installStaticAssets());
 });
 
-self.addEventListener('activate', () => {
+self.addEventListener('activate', (event) => {
   console.log(`[Service Worker] activate event lifecycle!`);
+  event.waitUntil(cacheCleanup());
   return self.clients.claim(); // claim all tabs
 });
 
@@ -27,8 +15,35 @@ self.addEventListener('fetch', async (event) => {
   event.respondWith(cacheFirst(event.request));
 });
 
+const CACHE_VERSION_KEY = 'sw-cache-v5';
+
+async function installStaticAssets() {
+  return caches
+    .open(CACHE_VERSION_KEY)
+    .then((cache) =>
+      cache.addAll([
+        'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.fluid.classless.min.css',
+        'https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm',
+        './',
+        './index.html',
+        './helpers/database.js',
+        './helpers/install-sw.js',
+        './install-data/index.js',
+        './app.js',
+      ])
+    );
+}
+
+async function cacheCleanup() {
+  const cacheKeys = await caches.keys();
+  const outdatedCache = (cacheKey) => cacheKey !== CACHE_VERSION_KEY;
+  const purge = (cacheKey) => caches.delete(cacheKey);
+  cacheKeys.filter(outdatedCache).forEach(purge);
+  return true;
+}
+
 async function cacheFirst(request) {
-  const cache = await caches.open('sw-cache-v1');
+  const cache = await caches.open(CACHE_VERSION_KEY);
   const response = await cache.match(request);
   if (response) {
     return response;
